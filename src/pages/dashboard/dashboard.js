978be33ec90dfb7ef9137e794ab123b379d2a43e
Muhammad Aadil <adilbanaras@gmail.com>
2020-07-18 16:48:43 +0400

    some css fix

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -7,14 +7,14 @@ import { Link } from 'react-router-dom'
 class Dashboard extends React.Component {
   render() {
     const auth = checkAuth();
-    return <>
-            <Link  className={auth.is_staff? 'btn btn-success':'btn btn-success disabled'} to='/dashboard/leads' >
+    return <div className="container text-center menu-container">
+            <Link  className={auth.is_staff? 'btn btn-success menu-card':'btn btn-success menu-card disabled'} to='/dashboard/leads' >
                 Leads
             </Link>
-            <Link  className={auth.is_super_admin?'btn btn-success':'btn btn-success disabled'} to='/dashboard/staff' >
+            <Link  className={auth.is_super_admin?'btn btn-success menu-card':'btn btn-success menu-card disabled'} to='/dashboard/staff' >
                 Staff
             </Link>
-    </>
+    </div>
   }
 }
 export default Dashboard
